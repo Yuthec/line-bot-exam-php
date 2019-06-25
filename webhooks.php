9e5fad7871461bf95ffc8668555409edc67974d0
Yuthec <51939712+Yuthec@users.noreply.github.com>
2019-06-25 21:19:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,49 +3,82 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
+include_once 'connf.php';
+
+
+$an = $pdo->prepare("SELECT (select count(an) from an_stat a where a.dchdate is null and ward = '0004' )as anward4
+");
+
+$an->execute();
+
+
+
+
+
+foreach ($an as $wor)  ;
+$anward4 = $wor['anward4'];
+
 $access_token = 'Pp+X6o0DyjM2PpSwiAKEjG/a0OyuCvAJt8iJsnCfTfOoAekSTLZqE0iKaOj6rorVcvXhMocDivS8k57i6Zz9vTP+FlFwGoIdHvbpvZeHXP9XWmCECyEUKak0G+8HNRAIUfOObeeM1NsDnLVbwCPYVQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+$arrayJson = json_decode($content, true);
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$access_token}";
+//รับข้อความจากผู้ใช้
+$message = $arrayJson['events'][0]['message']['text'];
+//รับ id ของผู้ใช้
+$id = $arrayJson['events'][0]['source']['userId'];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 
-			echo $result . "\r\n";
-		}
-	}
+
+
+#ตัวอย่าง Message Type "Text + Sticker"
+if($message == "สวัสดี"){
+   $arrayPostData['to'] = $id;
+   $arrayPostData['messages'][0]['type'] = "text";
+   $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+   $arrayPostData['messages'][1]['type'] = "sticker";
+   $arrayPostData['messages'][1]['packageId'] = "2";
+   $arrayPostData['messages'][1]['stickerId'] = "34";
+   pushMsg($arrayHeader,$arrayPostData);
 }
-echo "OK";
+ #ตัวอย่าง Message Type "Sticker"
+ else if($message == "ยอดเตียง"){
+   $arrayPostData['to'] = $id;
+  // $arrayPostData['messages'][0]['type'] = "sticker";
+  // $arrayPostData['messages'][0]['packageId'] = "2";
+  $arrayPostData['messages'][0]['type'] = "text";
+  $arrayPostData['messages'][0]['text'] = $anward4;
+  pushMsg($arrayHeader,$arrayPostData);
+}
+
+#ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+else if($message == "ลาก่อน"){
+	 $arrayPostData['to'] = $id;
+	$arrayPostData['messages'][0]['type'] = "text";
+	$arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+	$arrayPostData['messages'][1]['type'] = "sticker";
+	$arrayPostData['messages'][1]['packageId'] = "1";
+	$arrayPostData['messages'][1]['stickerId'] = "131";
+	pushMsg($arrayHeader,$arrayPostData);
+}
+function pushMsg($arrayHeader,$arrayPostData){
+   $strUrl = "https://api.line.me/v2/bot/message/push";
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL,$strUrl);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   $result = curl_exec($ch);
+   curl_close ($ch);
+}
+exit;
+?>
